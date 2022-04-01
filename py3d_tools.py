47a7f3643256e7dd1a31583dde8367dd0cb60c15
MSFTserver <MSFTserver@gmail.com>
2022-03-31 17:27:47 -0700

    add get_device definition

--- py3d_tools.py
+++ py3d_tools.py
@@ -1439,7 +1439,7 @@ class FoVPerspectiveCameras(CamerasBase):
 ##  ██║  ██║██╔══╝  ██╔══╝  ██║██║╚██╗██║██║   ██║   ██║██║   ██║██║╚██╗██║╚════██║  ##
 ##  ██████╔╝███████╗██║     ██║██║ ╚████║██║   ██║   ██║╚██████╔╝██║ ╚████║███████║  ##
 ##  ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝  ##
-#######################################################################################                                                                               
+#######################################################################################
 
 def make_device(device: Device) -> torch.device:
     """
@@ -1457,6 +1457,29 @@ def make_device(device: Device) -> torch.device:
         # In that case, we fix to that device
         device = torch.device(f"cuda:{torch.cuda.current_device()}")
     return device
+
+def get_device(x, device: Optional[Device] = None) -> torch.device:
+    """
+    Gets the device of the specified variable x if it is a tensor, or
+    falls back to a default CPU device otherwise. Allows overriding by
+    providing an explicit device.
+    Args:
+        x: a torch.Tensor to get the device from or another type
+        device: Device (as str or torch.device) to fall back to
+    Returns:
+        A matching torch.device object
+    """
+
+    # User overrides device
+    if device is not None:
+        return make_device(device)
+
+    # Set device based on input tensor
+    if torch.is_tensor(x):
+        return x.device
+
+    # Default device is cpu
+    return torch.device("cpu")
 
 def _axis_angle_rotation(axis: str, angle: torch.Tensor) -> torch.Tensor:
     """
